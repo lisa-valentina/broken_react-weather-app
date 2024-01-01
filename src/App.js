71fb69d5ada09a222232d8c7cffcb6287b6e1774
lisa-valentina <lisa.riedel@gmail.com>
2024-01-01 16:35:16 +0100

    updated typo on noreferrer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
+import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
+        <Weather />
         <footer>
           This project was coded by Lisa Valentina Riedel and is open sourced on{" "}
           <a
             href="https://github.com/lisa-valentina/react-weather-app"
             target={"_blank"}
-            rel={"norefferer"}
+            rel={"noreferrer"}
           >
             GitHub
           </a>
