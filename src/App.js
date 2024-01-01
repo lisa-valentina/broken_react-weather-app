51b214b7e4e932000a3b33a6e8d516f2b7b795ce
lisa-valentina <lisa.riedel@gmail.com>
2024-01-01 15:51:21 +0100

    Added GitHub Repository URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Lisa Valentina Riedel and is open sourced on{" "}
+        <a
+          href="https://github.com/lisa-valentina/react-weather-app"
+          target={"_blank"}
+        >
+          GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
