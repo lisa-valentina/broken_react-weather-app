4a56f6f0eb6078aff0b02f4aa597c455bd8a18c0
lisa-valentina <lisa.riedel@gmail.com>
2024-01-01 18:59:22 +0100

    image alignment correction

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,10 @@
 import React from "react";
+import axios from "axios";
 import "./Weather.css";
 export default function Weater() {
+  const apiKey = "c6ccad355ceda3eadc4ad39e3b374f54";
+  let cityName = "Pinneberg";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
   return (
     <div className="Weather">
       <form>
@@ -22,24 +26,19 @@ export default function Weater() {
           </div>
         </div>
       </form>
-      <h1>Hamburg</h1>
+      <h1>Pinneberg</h1>
       <ul>
         <li>Monday 16:13</li>
         <li>Rain</li>
       </ul>
       <div className="row mt-3">
         <div className="col-6">
-          <div className="clearfix">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/rain_light.png"
-              alt="rain"
-              className="float-left"
-            ></img>
-            <div className="float-left">
-              <span className="temperature">7</span>{" "}
-              <span className="unit"> °C</span>
-            </div>
-          </div>
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/rain_light.png"
+            alt="rain"
+          ></img>
+          <span className="temperature">7</span>{" "}
+          <span className="unit"> °C</span>
         </div>
         <div className="col-6">
           <ul>
