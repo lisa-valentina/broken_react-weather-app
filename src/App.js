030dc5643de576d95c2c44260ba552418a780e3f
lisa-valentina <lisa.riedel@gmail.com>
2024-01-01 16:06:05 +0100

    included bootstrap

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Lisa Valentina Riedel and is open sourced on{" "}
-        <a
-          href="https://github.com/lisa-valentina/react-weather-app"
-          target={"_blank"}
-        >
-          GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Lisa Valentina Riedel and is open sourced on{" "}
+          <a
+            href="https://github.com/lisa-valentina/react-weather-app"
+            target={"_blank"}
+            rel={"norefferer"}
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
