318fbd1a111cde50bda3b5860993807902d24882
lisa-valentina <lisa.riedel@gmail.com>
2024-01-01 18:33:42 +0100

    added app styling

--- src/Weather.js
+++ src/Weather.js
@@ -4,25 +4,42 @@ export default function Weater() {
   return (
     <div className="Weather">
       <form>
-        <input
-          type="search"
-          placeholder="Enter a city"
-          className="form-control"
-        />
-        <input type="submit" value="Search" className="btn btn-primary" />
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city"
+              className="form-control"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
+          </div>
+        </div>
       </form>
       <h1>Hamburg</h1>
       <ul>
         <li>Monday 16:13</li>
         <li>Rain</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/rain_light.png"
-            alt="rain"
-          ></img>
-          7 °C
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/rain_light.png"
+              alt="rain"
+              className="float-left"
+            ></img>
+            <div className="float-left">
+              <span className="temperature">7</span>{" "}
+              <span className="unit"> °C</span>
+            </div>
+          </div>
         </div>
         <div className="col-6">
           <ul>
