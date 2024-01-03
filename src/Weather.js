5efd36f3142239dc5c1e4dd1bfc0c11505cb9c49
lisa-valentina <lisa.riedel@gmail.com>
2024-01-03 17:16:05 +0100

    added animated weather icon

--- src/Weather.js
+++ src/Weather.js
@@ -14,7 +14,7 @@ export default function Weather(props) {
       wind: response.data.wind.speed,
       city: response.data.name,
       humidity: response.data.main.humidity,
-      iconUrl: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      icon: response.data.weather[0].icon,
       description: response.data.weather[0].description,
       date: new Date(response.data.dt * 1000),
     });
---
+++ src/WeatherIcon.js
@@ -0,0 +1,26 @@
+import react from "react";
+import { WeatherSvg } from "weather-icons-animated";
+
+export default function WeatherIcon(props) {
+  const codeMapping = {
+    "01d": "sunny",
+    "01n": "clear-night",
+    "02d": "partly-cloudy",
+    "02n": "partly-cloudy",
+    "03d": "partly-cloudy",
+    "03n": "partly-cloudy",
+    "04d": "cloudy",
+    "04n": "cloudy",
+    "09d": "rainy",
+    "09n": "rainy",
+    "10d": "pouring",
+    "10n": "pouring",
+    "11d": "lightning",
+    "11n": "lightning",
+    "13d": "snowy",
+    "13n": "snowy",
+    "50d": "fog",
+    "50n": "fog",
+  };
+  return <WeatherSvg state={codeMapping} width={100} height={100} />;
+}
